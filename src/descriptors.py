b5b5309477e39bf83cb73fcfe6a9adc363842b4e
chubinho <andrewchubinets07@yandex.ru>
2026-03-22 11:56:19 +0300

    add StatusDescription

--- src/descriptors.py
+++ src/descriptors.py
@@ -8,7 +8,7 @@ class IdDescriptor:
         Сохраняет имя атрибута.
         Вызывается автоматически при создании класса-владельца
         """
-        self.id = '_' + id
+        self.id = '_' + name
 
     def __set__(self, obj: object, value: int) -> None:
         """
@@ -20,9 +20,9 @@ class IdDescriptor:
             TypeError: Если значение не целое число
             ValueError: Если значение <= 0
         """
-        if not isinstance(id, int):
+        if not isinstance(value, int):
             raise TypeError("Id должен быть целый")
-        if id < 1:
+        if value < 1:
             raise ValueError("Id это целое число > 0")
         setattr(obj, self.id, value)
 
@@ -45,8 +45,9 @@ class PriorityDescriptor:
     Data Descriptor для валидации приоритета задачи.
     """
 
-    def __set_name__(self, owner: type, name: str) -> None:
+    def __set_name__(self, name: str, owner: type, ) -> None:
         """
+        Сохраняет имя атрибута.
         Вызывается автоматически при создании класса
 
         Args:
@@ -55,7 +56,7 @@ class PriorityDescriptor:
         """
         self.priority = '_' + name
 
-    def __set__(self, instance: object, value: int) -> None:
+    def __set__(self, obj: object, value: int) -> None:
         """
         Устанавливает значение приоритета с валидацией
 
@@ -71,7 +72,7 @@ class PriorityDescriptor:
             raise TypeError("Priority должен быть целым числом(от 1 до 10)")
         if not (1 <= value <= 10):
             raise ValueError("Priority должен быть в диапазоне от 1 до 10")
-        setattr(instance, self.priority, value)
+        setattr(obj, self.priority, value)
 
     def __get__(self, obj: object, owner: type = None) -> int | None:
         """
@@ -85,3 +86,44 @@ class PriorityDescriptor:
         if obj is None:
             return self
         return getattr(obj, self.priority, None)
+
+
+class StatusDescriptor:
+    def __set_name__(self, name: str, owner: type) -> None:
+        """
+        Сохраняет имя атрибута.
+        Вызывается автоматически при создании класса-владельца.
+        Args:
+            owner: Класс-владелец дескриптора
+            name: Имя атрибута в классе
+        """
+        self.status = '_' + name
+
+    def __set__(self, obj: object, value: str) -> None:
+        """
+        Устанавливает значение статуса с валидацией
+        Args:
+            obj: Экземпляр объекта
+            value: Значение статуса
+        Raises:
+            TypeError: Если значение не строка
+            ValueError: Если статус не входит в список допустимых
+        """
+        if not isinstance(value, str):
+            raise TypeError("Status должен быть строкой")
+        if value not in ["in_progress", "done", "cancelled", "paused", "new"]:
+            raise ValueError(f"Статус {value} недопустим")
+        setattr(obj, self.status, value)
+
+    def __get__(self, obj: object, owner: type = None) -> str | None:
+        """
+        Возвращает значение статуса
+        Args:
+            obj: Экземпляр объекта 
+            objtype: Класс-владелец
+        Returns:
+            Значение статуса или сам дескриптор
+        """
+        if obj is None:
+            return self
+        return getattr(obj, self.status, None)
